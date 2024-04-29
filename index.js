0a15ba110fbd2322ac8be29a6ec24bd37217df1b
SaujanBindukar <34705432+SaujanBindukar@users.noreply.github.com>
2024-04-29 21:30:29 +0545

    used query payloads

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/sample', (req, res) => {
+
+    //sending response to client
   res.send('Hello World! from my computer')
+})
+
+app.get('/users', (req, res) => {
+  const username= req.query.username;
+
+
+  //sending response to client
+res.send('Hello users here ' +username);
 })
 
 app.listen(port, () => {
