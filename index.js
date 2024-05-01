490666aafd41b1f646cca3240eacb60180928e37
SaujanBindukar <34705432+SaujanBindukar@users.noreply.github.com>
2024-05-01 18:32:46 +0545

    use express middle ware to convert request data into JSON

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//middle ware-> convert request into json
+app.use(express.json());
+
 app.get('/sample', (req, res) => {
 
     //sending response to client
@@ -10,12 +13,22 @@ app.get('/sample', (req, res) => {
 
 app.get('/users', (req, res) => {
   const username= req.query.username;
-
-
   //sending response to client
 res.send('Hello users here ' +username);
+})
+
+
+app.post('/users',(req,res)=>{
+
+  const myQuery= req.query.param;
+  console.log(myQuery);
+
+  res.json(req.body);
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
